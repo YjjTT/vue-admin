e4ee74ee495467d06885c189d29b3781e18e9017
YjjTT <yjjtt@tempot.com>
2020-07-13 23:14:30 +0800

    login 页面

--- vue-admin/src/views/Login/index.vue
+++ vue-admin/src/views/Login/index.vue
@@ -1,19 +1,60 @@
 <template>
   <div id="login">
-    这里是登录页
-    <el-button>按钮</el-button>
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li v-for="item in menuTab"
+            :key="item.txt"
+            :class="{'current': item.current}"
+            @click="toggleMenu(item)">{{item.txt}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      menuTab: [
+        {txt: '登录', current: true},
+        {txt: '注册', current: false}
+      ]
+    }
+  },
+  methods: {
+    toggleMenu(data) {
+      this.menuTab.forEach(element => {
+        element.current = false
+      })
+      data.current = true
+    }
+  }
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
     #login {
         background-color: $color_main;
         height: 100vh;
     }
+  .login-wrap {
+    width: 330px;
+    margin: auto;
+    .menu-tab {
+      text-align: center;
+      li {
+        display: inline-block;
+        width: 88px;
+        line-height: 36px;
+        font-size: 14px;
+        color: #ffffff;
+        border-radius: 2px;
+        cursor: pointer;
+      }
+      .current {
+        background-color: rgba(0, 0, 0, .1);
+      }
+    }
+  }
 </style>
