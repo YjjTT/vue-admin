91bd3bc73d5597b515bebaf890ee01f29ab2c3a1
YjjTT <yjjtt@tempot.com>
2020-07-13 22:51:31 +0800

    路由设置 scss配置文件

---
+++ vue-admin/src/views/Login/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="login">
+    这里是登录页
+    <el-button>按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<style lang="scss">
+    #login {
+        background-color: $color_main;
+        height: 100vh;
+    }
+</style>
