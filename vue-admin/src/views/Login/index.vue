90acee2cab29a618873ba5e64f4c83e68edfdf09
YjjTT <yjjtt@tempot.com>
2020-07-14 22:30:09 +0800

    form 验证`

--- vue-admin/src/views/Login/index.vue
+++ vue-admin/src/views/Login/index.vue
@@ -7,6 +7,30 @@
             :class="{'current': item.current}"
             @click="toggleMenu(item)">{{item.txt}}</li>
       </ul>
+      <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="login-form">
+        <el-form-item prop="username" class="item-form">
+          <label>邮箱</label>
+          <el-input type="text" v-model="ruleForm.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item prop="password" class="item-form">
+          <label>密码</label>
+          <el-input type="password" v-model="ruleForm.password" autocomplete="off" minlength="6" maxlength="20"></el-input>
+        </el-form-item>
+        <el-form-item prop="code" class="item-form">
+          <label>验证码</label>
+          <el-row :gutter="10">
+            <el-col :span="15">
+              <el-input v-model.number="ruleForm.code" minlength="6" maxlength="6"></el-input>
+            </el-col>
+            <el-col :span="9">
+              <el-button type="success" class="block">获取验证码</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="login-btn block" type="danger" @click="submitForm('ruleForm')">提交</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
@@ -15,11 +39,57 @@
 export default {
   name: "Login",
   data() {
+    const validateUsername = (rule, value, callback) => {
+      let reg = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+      if (value === '') {
+        callback(new Error('请输入用户名'));
+      } else if (!reg.test(value)){
+        callback(new Error('用户名格式有误'));
+      } else {
+        callback();
+      }
+    }
+    const validatePassword = (rule, value, callback) => {
+      let reg = /^(?!\D+$)(?![^a-zA-Z]+$)\S{6,20}$/
+      if (value === '') {
+        callback(new Error('请输入密码'));
+      } else if (!reg.test(value)) {
+        callback(new Error('密码为6-20位字母加数字组合'));
+      } else {
+        callback();
+      }
+    }
+    const validateCode = (rule, value, callback) => {
+      let reg = /^[a-z0-9]{6}$/
+      if (value === '') {
+        callback(new Error('请输入验证码'));
+      } else if (!reg.test(value)) {
+        callback(new Error('验证码格式有误'));
+      } else {
+        callback();
+      }
+    }
     return {
       menuTab: [
         {txt: '登录', current: true},
         {txt: '注册', current: false}
-      ]
+      ],
+      ruleForm: {
+        username: '',
+        checkPass: '',
+        age: ''
+      },
+      rules: {
+        username: [
+          { validator: validateUsername, trigger: 'blur' }
+        ],
+        password: [
+          { validator: validatePassword, trigger: 'blur' }
+        ],
+        code: [
+          { validator: validateCode, trigger: 'blur' }
+        ]
+      }
     }
   },
   methods: {
@@ -28,33 +98,21 @@ export default {
         element.current = false
       })
       data.current = true
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      })
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-    #login {
-        background-color: $color_main;
-        height: 100vh;
-    }
-  .login-wrap {
-    width: 330px;
-    margin: auto;
-    .menu-tab {
-      text-align: center;
-      li {
-        display: inline-block;
-        width: 88px;
-        line-height: 36px;
-        font-size: 14px;
-        color: #ffffff;
-        border-radius: 2px;
-        cursor: pointer;
-      }
-      .current {
-        background-color: rgba(0, 0, 0, .1);
-      }
-    }
-  }
+@import "./index.scss";
 </style>
