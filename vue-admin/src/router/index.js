1489f629c2afc77af851b36f93e8b9d3b22fb481
YjjTT <yjjtt@tempot.com>
2020-08-03 18:28:10 +0800

    信息列表页面

--- vue-admin/src/router/index.js
+++ vue-admin/src/router/index.js
@@ -1,96 +1,96 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-Vue.use(VueRouter);
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
 import Layout from '@/views/Layout'
 
 const routes = [
-  {
-    path: "/",
-    redirect: "login",
-    hidden: true,
-    meta: {
-      name: '主页'
-    }
-  },
-  {
-    path: "/login",
-    name: "Login",
-    hidden: true,
-    meta: {
-      name: '登录'
-    },
-    component: () => import("../views/Login/index.vue")
-  },
-  {
-    path: "/console",
-    name: "Console",
-    redirect: "index",
-    meta: {
-      name: '控制台',
-      icon: 'dashboard'
-    },
-    component: Layout,
-    children: [
-      {
-        path: "/index",
-        name: "Index",
+    {
+        path: '/',
+        redirect: 'login',
+        hidden: true,
         meta: {
-          name: '首页'
+            name: '主页',
         },
-        component: () => import("../views/Console/index.vue")
-      }
-    ]
-  },
-  {
-    path: "/info",
-    name: "Info",
-    meta: {
-      name: '信息管理',
-      icon: 'msg'
     },
-    component: Layout,
-    children: [
-      {
-        path: "/infoList",
-        name: "InfoList",
+    {
+        path: '/login',
+        name: 'Login',
+        hidden: true,
         meta: {
-          name: '信息列表'
+            name: '登录',
         },
-        component: () => import("../views/Info/list.vue")
-      },
-      {
-        path: "/infoCategory",
-        name: "InfoCategory",
-        meta: {
-          name: '信息分类'
-        },
-        component: () => import("../views/Info/category.vue")
-      }
-    ]
-  },
-  {
-    path: "/user",
-    name: "User",
-    meta: {
-      name: '用户管理',
-      icon: 'user'
+        component: () => import('../views/Login/index.vue'),
     },
-    component: Layout,
-    children: [
-      {
-        path: "/userList",
-        name: "UserList",
+    {
+        path: '/console',
+        name: 'Console',
+        redirect: 'index',
         meta: {
-          name: '用户列表'
+            name: '控制台',
+            icon: 'dashboard',
         },
-        component: () => import("../views/User/list.vue")
-      }
-    ]
-  }
-];
+        component: Layout,
+        children: [
+            {
+                path: '/index',
+                name: 'Index',
+                meta: {
+                    name: '首页',
+                },
+                component: () => import('../views/Console/index.vue'),
+            },
+        ],
+    },
+    {
+        path: '/info',
+        name: 'Info',
+        meta: {
+            name: '信息管理',
+            icon: 'msg',
+        },
+        component: Layout,
+        children: [
+            {
+                path: '/infoList',
+                name: 'InfoList',
+                meta: {
+                    name: '信息列表',
+                },
+                component: () => import('../views/Info/index.vue'),
+            },
+            {
+                path: '/infoCategory',
+                name: 'InfoCategory',
+                meta: {
+                    name: '信息分类',
+                },
+                component: () => import('../views/Info/category.vue'),
+            },
+        ],
+    },
+    {
+        path: '/user',
+        name: 'User',
+        meta: {
+            name: '用户管理',
+            icon: 'user',
+        },
+        component: Layout,
+        children: [
+            {
+                path: '/userList',
+                name: 'UserList',
+                meta: {
+                    name: '用户列表',
+                },
+                component: () => import('../views/User/list.vue'),
+            },
+        ],
+    },
+]
 
 const router = new VueRouter({
-  routes
-});
+    routes,
+})
 
-export default router;
+export default router
--- vue-admin/src/views/Info/index.vue
+++ vue-admin/src/views/Info/index.vue
@@ -1,10 +1,121 @@
-<template></template>
+<template>
+  <div>
+      <el-row :gutter="16">
+        <el-col :span="3">
+          <div class="label-wrap category">
+            <label for>类型:</label>
+            <div class="wrap-content">
+              <el-select v-model="value" placeholder="请选择" style="width: 100%;">
+                <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                ></el-option>
+              </el-select>
+            </div>
+          </div>
+        </el-col>
+
+        <el-col :span="7">
+          <div class="label-wrap date">
+            <label for>日期: &nbsp;&nbsp;</label>
+            <div class="wrap-content">
+              <el-date-picker
+                v-model="value1"
+                type="daterange"
+                style="width: 100%;"
+                range-separator="至"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期"
+              >
+              </el-date-picker>
+            </div>
+          </div>
+        </el-col>
+
+        <el-col :span="3">
+          <div class="label-wrap keyword">
+            <label for>关键字:</label>
+            <div class="wrap-content">
+              <el-select v-model="value" placeholder="请选择" style="width: 100%;">
+                <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                ></el-option>
+              </el-select>
+            </div>
+          </div>
+        </el-col>
+
+        <el-col :span="3">
+          <el-input style="width: 100%;" placeholder="请输入内容"></el-input>
+        </el-col>
+
+        <el-col :span="2">
+          <el-button style="width: 100%;" type="danger">搜索</el-button>
+        </el-col>
+
+        <el-col :span="3">&nbsp;</el-col>
+
+        <el-col :span="2">
+          <el-button style="width: 100%;" type="danger" class="pull-right">新增</el-button>
+        </el-col>
+      </el-row>
+  </div>
+</template>
 
 <script>
+import { reactive, ref } from "@vue/composition-api";
 export default {
-  name: "Index"
+  name: "Index",
+  setup() {
+    const options = reactive([
+      {
+        value: "选项1",
+        label: "黄金糕"
+      },
+      {
+        value: "选项2",
+        label: "双皮奶"
+      },
+      {
+        value: "选项3",
+        label: "蚵仔煎"
+      },
+      {
+        value: "选项4",
+        label: "龙须面"
+      },
+      {
+        value: "选项5",
+        label: "北京烤鸭"
+      }
+    ]);
+    const value = ref("");
+    const value1 = ref("");
+    return {
+      options,
+      value,
+      value1
+    };
+  }
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+@import "../../styles/sassConfig.scss";
+.label-wrap {
+  &.category {
+    @include labelDom(left, 60, 40);
+  }
+  &.date {
+    @include labelDom(right, 93, 40);
+  }
+  &.keyword {
+    @include labelDom(right, 99, 40);
+  }
+}
 </style>
